ceba6b02249c1152cccd6410e4f96b4abe79d548
Adrian Likins <alikins@redhat.com>
2020-02-06 12:28:55 -0500

    Add admin pages for core models
    generated with django-extensions admin_generator

---
+++ pulpcore/app/admin_contenttypes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from pulpcore.app.models import (
+    ContentType,
+)
+
+
+@admin.register(ContentType)
+class ContentTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'app_label', 'model')
