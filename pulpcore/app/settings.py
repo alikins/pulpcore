76d79a80427830d53cd85418aa3a8a415b83361b
Adrian Likins <alikins@redhat.com>
2020-02-07 10:39:02 -0500

    whatever

--- pulpcore/app/settings.py
+++ pulpcore/app/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY = True
 INSTALLED_APPS = [
     # django stuff
     'django.contrib.admin',
-    'django.contric.admindocs',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
